afc8e1c609e41a7bafd61e14128b93d06b0c3726
Thomas Schaad <tom@avisec.ch>
2021-06-25 08:20:00 +0200

    Worng placed space in compile script

--- compile_avrdude.sh
+++ compile_avrdude.sh
@@ -12,7 +12,7 @@ patch1=patches/90-avrdude-linuxspi-V1.patch
 patch2=patches/100-linuxspi-Add-reset-pulse-according-to-AVR-programmin.patch
 patch3=patches/101-linuxspi-close-only-when-necessary.patch
 patch4=patches/102-linuxspi-Report-GPIO_GET_LINEHANDLE_IOCTL-errors.patch
-patch5= patches/103-linuxspi-Support-inverted-GPIO-pin.patch
+patch5=patches/103-linuxspi-Support-inverted-GPIO-pin.patch
 patch6=patches/104-linuxspi-Support-GPIO-uAPI-v2.patch
 
 
